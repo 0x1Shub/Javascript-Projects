46a19a7e0145530c7532b31b2956f77dcceceda7
0x1Shub <shubhamyeram13@gmail.com>
2024-08-21 19:32:45 +0530

    Solved: Javascript Interview questions

--- 01_Javascript Interview Questions/09_Car_object.js
+++ 01_Javascript Interview Questions/09_Car_object.js
@@ -1,3 +1,11 @@
 let car = {
   make: "Rolls-Royce",
+  model: "Phantom",
+  year: 2024,
 };
+
+car.startEngine = function () {
+  console.log("Engine Started");
+};
+
+car.startEngine();
---
+++ 01_Javascript Interview Questions/10_Even_num.js
@@ -0,0 +1,7 @@
+let arr = [12, 2346, 213, 45, 73, 322];
+
+function evenNum(arr) {
+  return arr.filter((num) => num % 2 === 0);
+}
+
+evenNum();
